19a0f8cfc8e5c59b3dc86807b3db4cbfa1079de4
Kines <guilhermealvarenga0124@gmail.com>
2026-09-23 21:02:06 -0300

    Refactor: Reduzida a complexidade não utilizada da classe

--- src/train.py
+++ src/train.py
@@ -1,33 +1,32 @@
 import pandas as pd
 
-from sklearn.pipeline import Pipeline
 from sklearn.linear_model import LogisticRegression
 
 from preprocessing import preprocess
 
 
-def train_model(df):
+def train_model(df, t_size=0.2):
 
     x_train, x_teste, y_train, y_teste, preprocessor = preprocess(
         df,
+        t_size=t_size
     )
 
-    model = Pipeline(
-        steps=[
-            ("preprocessor", preprocessor),
-            (
-                "model",
-                LogisticRegression(
-                    class_weight="balanced",
-                    max_iter=1000
-                )
-            )
-        ]
+    x_train_normalized = preprocessor.fit_transform(x_train)
+
+    x_teste_normalized = preprocessor.transform(x_teste)
+
+    model = LogisticRegression(
+        class_weight='balanced',
+        max_iter=1000
     )
 
-    model.fit(x_train, y_train)
+    model.fit(
+        x_train_normalized,
+        y_train
+    )
 
-    y_pred = model.predict(x_teste)
+    y_pred = model.predict(x_teste_normalized)
 
     return model, y_teste, y_pred
 
