76c09b273711123bfef468c7ea2755ed559e39d4
Kines <guilhermealvarenga0124@gmail.com>
2026-09-24 16:25:27 -0300

    Feat : Seleção de atributos aplicada na RF e RL

--- src/train.py
+++ src/train.py
@@ -5,20 +5,44 @@ from sklearn.ensemble import RandomForestClassifier
 
 from preprocessing import preprocess
 
-## Treinamento modelo de regressão logistica
-def train_logistic_regression(df, t_size=0.2):
+
+
+# LOGISTIC REGRESSION
+
+
+def train_logistic_regression(
+    df,
+    t_size=0.2,
+    threshold=0.50
+):
+
+    variaveis_remover = [
+        "gender",
+        "PhoneService",
+        "MultipleLines",
+        "PaperlessBilling"
+    ]
+
+    df = df.drop(
+        columns=variaveis_remover
+    )
 
     x_train, x_teste, y_train, y_teste, preprocessor = preprocess(
         df,
         t_size=t_size
     )
 
-    x_train_transformed = preprocessor.fit_transform(x_train)
+    x_train_transformed = preprocessor.fit_transform(
+        x_train
+    )
 
-    x_teste_transformed = preprocessor.transform(x_teste)
+    x_teste_transformed = preprocessor.transform(
+        x_teste
+    )
 
     model = LogisticRegression(
-        class_weight='balanced',
+        C=1,
+        class_weight="balanced",
         max_iter=1000
     )
 
@@ -27,29 +51,63 @@ def train_logistic_regression(df, t_size=0.2):
         y_train
     )
 
-    y_pred = model.predict(x_teste_transformed)
-    
+    # Probabilidade de churn
     y_prob = model.predict_proba(
-    x_teste_transformed
+        x_teste_transformed
     )[:, 1]
 
-    return model, preprocessor, y_teste, y_pred, y_prob
+    # Aplicação do threshold
+    y_pred = pd.Series(
+        y_prob >= threshold,
+        index=y_teste.index
+    ).map({
+        False: "No",
+        True: "Yes"
+    })
 
-## Treinamento Random forest
-def train_random_forest(df, t_size=0.2):
+    return (
+        model,
+        preprocessor,
+        y_teste,
+        y_pred,
+        y_prob
+    )
+
+## RANDOM FOREST
+
+def train_random_forest(
+    df,
+    t_size=0.2
+):
+
+    variaveis_remover = [
+        "gender",
+        "PhoneService",
+        "MultipleLines"
+    ]
+
+    df = df.drop(
+        columns=variaveis_remover
+    )
 
     x_train, x_teste, y_train, y_teste, preprocessor = preprocess(
         df,
         t_size=t_size
     )
 
-    x_train_transformado = preprocessor.fit_transform(x_train)
+    x_train_transformado = preprocessor.fit_transform(
+        x_train
+    )
 
-    x_teste_transformado = preprocessor.transform(x_teste)
+    x_teste_transformado = preprocessor.transform(
+        x_teste
+    )
 
     model = RandomForestClassifier(
+        n_estimators=200,
+        max_depth=7,
+        min_samples_leaf=5,
         class_weight="balanced",
-        max_depth=8,
         random_state=42
     )
 
@@ -58,18 +116,28 @@ def train_random_forest(df, t_size=0.2):
         y_train
     )
 
-    y_pred = model.predict(x_teste_transformado)
+    y_pred = model.predict(
+        x_teste_transformado
+    )
 
     y_prob = model.predict_proba(
         x_teste_transformado
     )[:, 1]
 
-    return model, preprocessor, y_teste, y_pred, y_prob
-
+    return (
+        model,
+        preprocessor,
+        y_teste,
+        y_pred,
+        y_prob
+    )
 
 if __name__ == "__main__":
 
-    df = pd.read_csv("data/processed/churn_modelagem.csv")
+    df = pd.read_csv(
+        "data/processed/churn_modelagem.csv"
+    )
 
     train_logistic_regression(df)
+
     train_random_forest(df)
